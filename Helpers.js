bd3dbc87becf55092133fe80d213c8bd4f143f5c
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2021-10-17 21:25:45 +0300

    Init project

---
+++ Helpers.js
@@ -0,0 +1,41 @@
+/**
+ * Finds largest dollar amount from email body.
+ * Returns null if no dollar amount is found.
+ *
+ * @param {Message} message An email message.
+ * @returns {String}
+ */
+ function getLargestAmount(message) {
+    return 'TODO';
+  }
+  
+  /**
+   * Determines date the email was received.
+   *
+   * @param {Message} message An email message.
+   * @returns {String}
+   */
+  function getReceivedDate(message) {
+    return 'TODO';
+  }
+  
+  /**
+   * Determines expense description by joining sender name and message subject.
+   *
+   * @param {Message} message An email message.
+   * @returns {String}
+   */
+  function getExpenseDescription(message) {
+    return 'TODO';
+  }
+  
+  /**
+   * Determines most recent spreadsheet URL.
+   * Returns null if no URL was previously submitted.
+   *
+   * @returns {String}
+   */
+  function getSheetUrl() {
+    return 'TODO';
+  }
+  
---
+++ scheduler.js
@@ -0,0 +1,15 @@
+function testScheduler() {
+  
+}
+
+function getCurrentEvents() {
+
+var today = new Date();
+
+  var events = CalendarApp.getDefaultCalendar().getEventsForDay(today, {search: 'meeting'});
+
+}
+
+function findSpot() {
+
+}

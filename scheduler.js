1aa0258ce787bb9eeacaa2339fcb498a08fda185
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2021-10-28 23:57:19 +0300

    Recursively find all avail spots

--- scheduler.js
+++ scheduler.js
@@ -10,8 +10,10 @@ function testScheduler() {
 
   const sortedEvents = sortEvents(events.slice(0));
   const offset = new Date((new Date()).getTime() + 11 * 60 * 60 * 1000 + 24 * 60 * 60 * 1000);
-  const found = findSpot(sortedEvents, offset, 3 * 60 * 60 * 1000);
-  console.log(`Prev: ${found.pE?.getTitle()}\nfound: ${found.caret}\nNext: ${found.aE?.getTitle()}`);
+  const founds = findSpot(sortedEvents, offset, 3 * 60 * 60 * 1000);
+  founds.forEach( (found) => {
+      console.log(`Prev: ${found.pE?.getTitle()}\nfound: ${found.caret}\nNext: ${found.aE?.getTitle()}`);
+  });
 }
 
 function findSchedule(duration = 30 * 60 * 1000 /* 30mins */) {
@@ -46,8 +48,8 @@ function sortEvents(events) {
 }
 
 function findSpot(events, after, duration_ms, ignoreAllDayEvents=true) {
-  const foundCtx = events.reduce((ctx, event) => {
-    const {pE, caret,aE} = ctx;
+  const foundCtx = events.reduce((ctx, event, index, array) => {
+    const [{pE, caret, aE}, ...others] = ctx;
 
     if (aE) {
       // next event is already found so the search is over, just return the current context
@@ -66,25 +68,47 @@ function findSpot(events, after, duration_ms, ignoreAllDayEvents=true) {
 
     // check if event is earlier or colliding
     if (eventStartTime < caretTime) {
-      return {
-        pE:event,
-        caret: (eventEndTime <= caretTime)
-          ? caret // event ends before the caret so, caret is still the later
-          : event.getEndTime(), // event ends after the caret so, bump the caret to the end of the event
-        aE
-      };
+      return [
+        {
+          pE:event,
+          caret: (eventEndTime <= caretTime)
+            ? caret // event ends before the caret so, caret is still the later
+            : event.getEndTime(), // event ends after the caret so, bump the caret to the end of the event
+          aE
+        },
+        ...others
+      ];
     }
     // event starts after the caret
 
     // check if there's enough time between the caret and the event
     if ((eventStartTime - caretTime) < duration_ms) {
       // there isn't enough time so, move caret to the end of the event
-      return {pE:event, caret:(event.getEndTime()), aE};
+      return [
+        {
+          pE:event,
+          caret:(event.getEndTime()),
+          aE
+        },
+        ...others
+      ];
     } else {
       // there's enough space until the event so, mark the event as next.
-      return {pE, caret, aE:event};
+
+      // but first, let's find the next available spot (recursively)
+      const nextOptions = findSpot(array.slice(index), event.getEndTime(), duration_ms, ignoreAllDayEvents);
+      others.push(...nextOptions);
+
+      return [
+        {
+          pE,
+          caret,
+          aE:event
+        },
+        ...others
+      ];
     }
-  }, {caret:after});
+  }, [{caret:after}]);
 
   return foundCtx;
 }

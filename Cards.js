1de345684fe4b77ee1d50e62866a1e9d8a834299
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2021-10-30 01:08:11 +0300

    Some cleanup

--- Cards.js
+++ Cards.js
@@ -1,5 +1,3 @@
-// var FIELDNAMES = ['Date', 'Amount', 'Description', 'Spreadsheet URL'];
-
 function createSchedulerCard(opt_prefills, opt_status) {
   var card = CardService.newCardBuilder();
   // card.setHeader(CardService.newCardHeader().setTitle('Schedule a task'));
@@ -100,74 +98,9 @@ function createFormSection(section, opt_prefills) {
       dateTimePicker.setValueInMsSinceEpoch(opt_prefills.date_time.getTime());
       dateTimeLabel.setText(opt_prefills.date_time.toString());
     }
-      // .setValueInMsSinceEpoch(1514775600);
-      // // EDT time is 5 hours behind UTC.
-      // .setTimeZoneOffsetInMins(-5 * 60)
-      // .setOnChangeAction(CardService.newAction()
-      // .setFunctionName("handleDateTimeChange"));
+
     section.addWidget(dateTimeLabel);
     section.addWidget(dateTimePicker);
 
-    // for (var i = 0; i < inputNames.length; i++) {
-    //     var widget = CardService.newTextInput()
-    //     .setFieldName(inputNames[i])
-    //     .setTitle(inputNames[i]);
-    //     if (opt_prefills && opt_prefills[i]) {
-    //         widget.setValue(opt_prefills[i]);
-    //     }
-    //     section.addWidget(widget);
-    // }
     return section;
 }
-
-/**
- * Creates the main card users see with form inputs to log expenses.
- * Form can be prefilled with values.
- *
- * @param {String[]} opt_prefills Default values for each input field.
- * @param {String} opt_status Optional status displayed at top of card.
- * @returns {Card}
- */
-function createExpensesCard(opt_prefills, opt_status) {
-  var card = CardService.newCardBuilder();
-  card.setHeader(CardService.newCardHeader().setTitle('Log Your Expense'));
-  
-  if (opt_status) {
-    if (opt_status.indexOf('Error: ') == 0) {
-      opt_status = '<font color=\'#FF0000\'>' + opt_status + '</font>';
-    } else {
-      opt_status = '<font color=\'#228B22\'>' + opt_status + '</font>';
-    }
-    var statusSection = CardService.newCardSection();
-    statusSection.addWidget(CardService.newTextParagraph()
-      .setText('<b>' + opt_status + '</b>'));
-    card.addSection(statusSection);
-  }
-  
-  var formSection = createFormSection(CardService.newCardSection(),
-                                      FIELDNAMES, opt_prefills);
-  card.addSection(formSection);
-  
-  return card;
-}
-
-// /**
-//  * Creates form section to be displayed on card.
-//  *
-//  * @param {CardSection} section The card section to which form items are added.
-//  * @param {String[]} inputNames Names of titles for each input field.
-//  * @param {String[]} opt_prefills Default values for each input field.
-//  * @returns {CardSection}
-//  */
-// function createFormSection(section, inputNames, opt_prefills) {
-//   for (var i = 0; i < inputNames.length; i++) {
-//     var widget = CardService.newTextInput()
-//       .setFieldName(inputNames[i])
-//       .setTitle(inputNames[i]);
-//     if (opt_prefills && opt_prefills[i]) {
-//       widget.setValue(opt_prefills[i]);
-//     }
-//     section.addWidget(widget);
-//   }
-//   return section;
-// }
--- GetContextualAddOn.js
+++ GetContextualAddOn.js
@@ -22,37 +22,34 @@
  * @param {Object} event Event containing the message ID and other context.
  * @returns {Card[]}
  */
- function getContextualAddOn(event) {
-    var message = getCurrentMessage(event);
-    const title = getTitle(message);
+function getContextualAddOn(event) {
+  var message = getCurrentMessage(event);
+  const title = getTitle(message);
 
-    const now = new Date();
-    const until = oneWeekFrom(now);
-    const events = getEvents(CALENDARS, now, until);
-    console.log(events[0].getTitle());
-    console.log(JSON.stringify(events, null, 4));
+  const now = new Date();
+  const until = oneWeekFrom(now);
+  const events = getEvents(CALENDARS, now, until);
 
-    var prefills = {
-        title: title,
-        events: events,
-        startTimeMS: now.getTime(),
-        untilTimeMS: until.getTime(),
-        date_time: findAndMarkSchedule(title, events, DEFAULT_DURATION_MS, now, until)
-    };
-    var card = createSchedulerCard(prefills);
+  var prefills = {
+      title: title,
+      events: events,
+      startTimeMS: now.getTime(),
+      untilTimeMS: until.getTime(),
+      date_time: findAndMarkSchedule(title, events, DEFAULT_DURATION_MS, now, until)
+  };
+  var card = createSchedulerCard(prefills);
+
+  return [card.build()];
+}
   
-    return [card.build()];
-  }
-  
-  /**
-   * Retrieves the current message given an action event object.
-   * @param {Event} event Action event object
-   * @return {Message}
-   */
-  function getCurrentMessage(event) {
-    var accessToken = event.messageMetadata.accessToken;
-    var messageId = event.messageMetadata.messageId;
-    GmailApp.setCurrentMessageAccessToken(accessToken);
-    return GmailApp.getMessageById(messageId);
-  }
-  
+/**
+ * Retrieves the current message given an action event object.
+ * @param {Event} event Action event object
+ * @return {Message}
+ */
+function getCurrentMessage(event) {
+  var accessToken = event.messageMetadata.accessToken;
+  var messageId = event.messageMetadata.messageId;
+  GmailApp.setCurrentMessageAccessToken(accessToken);
+  return GmailApp.getMessageById(messageId);
+}
